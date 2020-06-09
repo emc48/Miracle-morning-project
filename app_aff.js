c005b2c3637338f2ec1efe9b5a2266b4f40f4fc5
emc48 <emc48@bath.ac.uk>
2020-06-09 20:12:25 +0100

    Initial commit

---
+++ app_aff.js
@@ -0,0 +1,27 @@
+alert("Affrimations are a great way to remind yourself of who you are and what you want. Let's give it a go!")
+
+function userchoice_powerful() {
+        document.getElementById("powerful_aff").style.color = "purple";
+}
+
+function userchoice_grounded() {
+    document.getElementById("grounded_aff").style.color = "purple";
+}
+
+function userchoice_joyful() {
+    document.getElementById("joyful_aff").style.color = "purple";
+}
+
+function userchoice_calm() {
+    document.getElementById("calm_aff").style.color = "purple";
+}
+
+//function userchoice_own() {
+    //alert("Let's go! Pick 3 affrimations for today!")
+    //var choice_1 = prompt("Number 1")
+    //var choice_2 = prompt("Number 2")
+    //var choice_3 = prompt("Number 3")
+    //var newPara = document.createElement("Number1" + "Number2" + "Number3")
+    //document.Aff_container.appendChild(newPara)
+    //document.getElementById("own_aff").style.color = "purple";
+//}
---
+++ app_index.js
@@ -0,0 +1,14 @@
+//alert("Welcome to my miracle morning! Let's have the best day we can!")
+
+//var name = prompt("What is your name?")
+//console.log(name)
+
+alert("Welcome to my miracle morning!");
+
+var name = prompt("What is your name?");
+
+    if (name == null || name == "") {
+  alert("Please reload the page and try again!");
+    } else {
+  alert("Hello " + name + "! Let's have a great day!");
+    }
