156bb96cecee0583a90ca0c681848e8583a46871
emc48 <emc48@bath.ac.uk>
2020-06-10 18:56:21 +0100

    Update app_index.js

--- app_index.js
+++ app_index.js
@@ -7,7 +7,7 @@ alert("Welcome to my miracle morning!");
 
 var name = prompt("What is your name?");
 
-    if (name == null || name == "") {
+    if (name == "null" || name == "") {
   alert("Please reload the page and try again!");
     } else {
   alert("Hello " + name + "! Let's have a great day!");
